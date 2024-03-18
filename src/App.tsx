0a34d188d740025c32dddb93db4ab58f2062ebd8
John Larson <jl@buildmidwestern.com>
2024-03-18 05:38:45 -0500

    feat: updated prettier, installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className='min-h-screen w-screen flex flex-col justify-between items-center bg-red-500'>
+			<div className='bg-blue-500 w-full h-full'>Nav</div>
+			<div className='bg-green-500 flex-1 w-full'>Hello</div>
+			<div className='bg-blue-500 w-full h-full'>Footer</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
